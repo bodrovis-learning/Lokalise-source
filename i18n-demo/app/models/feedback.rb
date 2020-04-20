c672380aa046801d47678c72aa248fc76628a672
bodrovis <bodrovis@protonmail.com>
2020-04-20 17:36:46 +0300

    bump

--- i18n-demo/app/models/feedback.rb
+++
@@ -1,2 +0,0 @@
-class Feedback < ApplicationRecord
-end
--- i18n-demo/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :feedbacks
-  root 'static_pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
